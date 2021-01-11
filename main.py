fd8f56112d828c635ecc5dab4d5db07bd369aae1
OGURCHINSKIY <41481736+OGURCHINSKIY@users.noreply.github.com>
2021-01-12 01:30:40 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,45 @@
+import asyncio
+from io import BytesIO
+from PIL import Image
+import pytesseract
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters.state import State, StatesGroup
+
+pytesseract.pytesseract.tesseract_cmd = '<full_path_to_your_tesseract_executable>'
+API_TOKEN = ''
+tags = ["Elon", "Musk", "BTC"]
+wait = 30
+
+bot = Bot(token=API_TOKEN)
+storage = MemoryStorage()
+dp = Dispatcher(bot, storage=storage)
+
+
+class States(StatesGroup):
+    new_member = State()
+
+
+@dp.message_handler(content_types="photo", state=States.new_member)
+async def album_handle(message: types.Message, state: FSMContext):
+    check = await is_musk(message)
+    if check:
+        await message.delete()
+
+
+@dp.message_handler(content_types='new_chat_members')
+async def new_chat_members(message: types.Message, state: FSMContext):
+    asyncio.get_event_loop().call_later(wait, asyncio.create_task, state.finish())
+    await States.new_member.set()
+
+
+async def is_musk(message: types.Message):
+    file = BytesIO()
+    await message.photo[-1].download(destination=file)
+    text = pytesseract.image_to_string(Image.open(file))
+    return all(j in text for j in tags)
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
